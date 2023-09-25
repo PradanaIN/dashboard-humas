5886da616dbb67f01df7015849fd2ec241227b6f
Novanni Indi Pradana <73351333+PradanaIN@users.noreply.github.com>
2023-09-25 07:27:02 +0700

    user list and profile

--- application/views/templates/auth_header.php
+++ application/views/templates/auth_header.php
@@ -6,7 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Login - SB Admin</title>
+        <title>Badan Pusat Statistik</title>
+		<link rel="icon" href="<?= base_url('assets/'); ?>img/logo.png" type="image/gif" sizes="25x25">
         <link href="<?= base_url("assets/dist/")?>css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
